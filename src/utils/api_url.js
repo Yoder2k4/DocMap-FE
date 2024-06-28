09758cabda3a5c3783cc63f1f9f0c5c2fbaccf81
Yash Sharma <22je1104@iitism.ac.in>
2024-06-28 23:31:00 +0530

    Changed API_BASE url

--- src/utils/api_url.js
+++ src/utils/api_url.js
@@ -1,5 +1,5 @@
 let API_BASE = 'http://localhost:3001';
 if(process.env.NODE_ENV === 'production') {
-    API_BASE = 'https://docmap-be.vercel.app';
+    API_BASE = 'https://docmap-be.onrender.com';
 }
 export default API_BASE;
